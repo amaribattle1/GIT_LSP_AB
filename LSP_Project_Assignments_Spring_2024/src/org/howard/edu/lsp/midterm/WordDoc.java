e78688bb67545581587cc63c1b2114cc7f7c120f
amaribattle1 <124077929+amaribattle1@users.noreply.github.com>
2024-03-14 19:11:09 -0400

    word doc for midterm

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/WordDoc.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/WordDoc.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class WordDoc {
 
 	public static void main(String[] args) {
-		String filePath = "documents/LSMidterm.docx";
+		String filePath = "documents/midterm-spring-2024-part1.docx";
 		File file = new File(filePath);
 		}
 
