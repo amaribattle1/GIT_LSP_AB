7204431b7f5f66346ee5aa4d0bd1121354bd630f
amaribattle1 <124077929+amaribattle1@users.noreply.github.com>
2024-03-14 13:40:03 -0400

    added file path

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/WordDoc.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/WordDoc.java
@@ -1,10 +1,12 @@
 package org.howard.edu.lsp.midterm;
 
+import java.io.File;
+
 public class WordDoc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String filePath = "documents/LSMidterm.docx";
+		File file = new File(filePath);
+		}
 
 	}
-
-}
--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question2/MidtermQuestion2.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question2/MidtermQuestion2.java
@@ -1,5 +1,7 @@
 package org.howard.edu.lsp.midterm.question2;
 
+
+
 class MidtermQuestion2 {
 
 }
