4f27e1e06a881fec35ef805c90ca27850411a1df
amaribattle1 <124077929+amaribattle1@users.noreply.github.com>
2024-05-02 19:38:44 -0400

    assignment 2

---
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/oopfinal/question2/BitcoinPayment.java
@@ -0,0 +1,19 @@
+package org.howard.edu.lsp.oopfinal.question2;
+
+
+interface PaymentStrategy {
+    void pay(double amount);
+}
+
+class BitcoinPayment implements PaymentStrategy {
+    private String bitcoinAddress;
+
+    public BitcoinPayment(String bitcoinAddress) {
+        this.bitcoinAddress = bitcoinAddress;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using Bitcoin address " + bitcoinAddress);
+    }
+}
